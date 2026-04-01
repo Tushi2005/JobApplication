a6111ab3c898014dac34a27ba5fc691033ee6dd6
Tuska Adam <tuskaadam2005@gmail.com>
2026-04-01 22:26:09 +0200

    feat: add IApplicationService interface and ApplicationService

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using JobApplication.Data;
+using JobApplication.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace JobApplication
@@ -15,6 +16,8 @@ namespace JobApplication
 
             builder.Services.AddDbContext<AppDbContext>(options =>
                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddScoped<IApplicationService, ApplicationService>();
 
             var app = builder.Build();
 
---
+++ Services/IApplicationService.cs
@@ -0,0 +1,13 @@
+﻿using JobApplication.Models;
+
+namespace JobApplication.Services
+{
+    public interface IApplicationService
+    {
+        Task<List<Application>> GetAllAsync(int userId);
+        Task<Application?> GetByIdAsync(int id, int userId);
+        Task<Application> CreateAsync(Application application);
+        Task<Application?> UpdateAsync(int id, Application application, int userId);
+        Task<bool> DeleteAsync(int id, int userId);
+    }
+}
