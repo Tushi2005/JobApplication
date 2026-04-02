515e0e80e7f8e66390838fc4119819c72579de67
Tuska Adam <tuskaadam2005@gmail.com>
2026-04-02 11:02:03 +0200

    feat: add Applications controller and DTOs

---
+++ DTOs/UpdateApplicationDto.cs
@@ -0,0 +1,15 @@
+using JobApplication.Models;
+
+namespace JobApplication.DTOs
+{
+    public class UpdateApplicationDto
+    {
+        public required string CompanyName { get; set; }
+        public required string Position { get; set; }
+        public ApplicationStatus Status { get; set; }
+        public DateTime AppliedAt { get; set; }
+        public DateTime? InterviewAt { get; set; }
+        public string? JobUrl { get; set; }
+        public string? Notes { get; set; }
+    }
+}
